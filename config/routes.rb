1e0d5eff2a3fe8bb3b3c2caf0694091ccea45e9f
Juan Jose Marinelli <juanjosemarinelli@gmail.com>
2014-09-17 15:21:59 -0300

    ?

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Desafiophilips::Application.routes.draw do
+  root 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
