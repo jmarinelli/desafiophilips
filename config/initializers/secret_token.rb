6f5b23b8e8bfb6bd65a35a8ed2f5c4ab86112cfe
Juan Jose Marinelli <juanjosemarinelli@gmail.com>
2014-09-17 15:02:22 -0300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Desafiophilips::Application.config.secret_key_base = '5e76d903eae817c8baaae162ae541b45c5fca001c097be2f35f6e9ff513977145661a26cda6276122fa3cbcc9c76132a2e1974ce179188c7093e3b24becebfeb'
