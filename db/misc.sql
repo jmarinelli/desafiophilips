fbb1b1f158233e4d153eeda47e95584e7644f508
Juan Jose Marinelli <juanjosemarinelli@gmail.com>
2014-09-30 21:46:12 -0300

    Cambio

--- db/misc.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO companies VALUES (1, 'Fravega');
-INSERT INTO companies VALUES (2, 'Garbarino');
-INSERT INTO companies VALUES (3, 'Compumundo');
-
-INSERT INTO positions VALUES(1, 'Vendedor');
-INSERT INTO positions VALUES(2, 'Sub-Gerente');
-INSERT INTO positions VALUES(3, 'Gerente');
